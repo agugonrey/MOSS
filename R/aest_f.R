b56abb11d60c3f436733335c23698f3dd8d4f448
agugonrey <agugonrey@gmail.com>
2020-05-19 19:41:34 -0400

    Add documentation.

--- R/aest_f.R
+++ R/aest_f.R
@@ -1,3 +1,12 @@
+#' Assign point color and shape aesthetics.
+#' 
+#' This function is called by functions within MOSS that plot stuff. 
+#' It simply assigns colors and shape to points based on input labels.
+#' @param x Character vector with labels or a numerical vector split in 'n.cat' categories.
+#' @param n.cat Number of categories to split vector 'x'. Numeric. Ignored if is(x,'character')=TRUE.
+#' @param option Controls color palette. One of the possible 'option' arguments for the 'viridis' function.
+#' @return A data.frame with labels as rownames and two columns representing point colors and shape.
+#' @export
 aest.f <- function(x,n.cat=2,option="D") {
   if(!requireNamespace("viridis",quietly = TRUE)) stop("Package 'viridis' needs to be installed.")
   if (is.numeric(x) == T) x <- ggplot2::cut_interval(x, n.cat)
