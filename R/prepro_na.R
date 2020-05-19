5713c40d613d763778a7d67ad827ac4d796a3cda
agugonrey <agugonrey@gmail.com>
2020-05-19 19:28:07 -0400

    Add documentation.

--- R/prepro_na.R
+++ R/prepro_na.R
@@ -1,3 +1,11 @@
+#' Number of missing values within a matrix.
+#' 
+#' This function is called by moss to count the number of missing values within the (extended) matrices.
+#' 
+#' Ment for objects of class 'matrix', 'FBM', or 'array'.
+#' @param X An object of class 'matrix', 'FBM', or 'array'.
+#' @return Returns total number of missing values in X.
+#' @export
 prepro_na <- function(X) {
   if(any(vapply(c("matrix","FBM","array"), function (x) inherits(X,x),TRUE))) {
     if (inherits(X, "FBM") == TRUE) {
