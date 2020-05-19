2365a8073a0dacf36559fb190ff1b1ed2bc157e8
agugonrey <agugonrey@gmail.com>
2020-05-19 19:19:19 -0400

    Fix trailing periods and added documentation.

--- R/prepro_sub.R
+++ R/prepro_sub.R
@@ -1,17 +1,27 @@
-prepro_sub <- function(X,scale.,norm.) {
+#' Scale and normalize columns of a matrix.
+#' 
+#' This function is called by moss to scale and normalize (extended) matrices.
+#' 
+#' Ment for objects of class 'matrix', 'FBM', or 'array'.
+#' @param X An object of class 'matrix', 'FBM', or 'array'.
+#' @param scale.arg Should we scale columns? Logical.
+#' @param norm.arg Should we normalize columns? Logical.
+#' @return A matrix with scaled and/or normalized columns.
+#' @export
+prepro_sub <- function(X,scale.arg,norm.arg) {
   if(any(vapply(c("matrix","FBM","array"), function (x) inherits(X,x),TRUE))) {
     if (inherits(X, "FBM") == TRUE) {
         bigstatsr::big_apply(X, function(x, ind) {
-        if (scale.) X[,ind] <- scale(x[,ind])
-        if (norm.) X[,ind] <- x[,ind] / sqrt(ncol(X))
+        if (scale.arg) X[,ind] <- scale(x[,ind])
+        if (norm.arg) X[,ind] <- x[,ind] / sqrt(ncol(X))
       },
       a.combine = 'c',
       ind = seq_len(ncol(X)),
       block.size = bigstatsr::block_size(ncol(X), 1))
     }
     else {
-      if (scale.) X <- scale(X)
-      if (norm.) X <- X / ncol(X)
+      if (scale.arg) X <- scale(X)
+      if (norm.arg) X <- X / ncol(X)
     }
   }
   else stop("Only objects of class 'array', 'matrix' or 'FBM' supported.")
