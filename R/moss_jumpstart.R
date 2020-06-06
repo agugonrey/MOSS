a7d8de9ab498e795b6f9ef000cc31ac3d7c1e882
Alexander Grueneberg <git@agrueneberg.info>
2020-06-06 10:35:26 -0400

    Fix wrong help page

--- R/moss_jumpstart.R
+++ R/moss_jumpstart.R
@@ -10,7 +10,7 @@ Maintainer:
   Agustin Gonzalez-Reymundez <gonza650@msu.edu>,                    
 
 Documentation:
-  Type 'help(MOSS)' for package overview and examples.
+  Type 'help(moss)' for package overview and examples.
 
 Type 'sim.data <- simulate_data()',
 to generate a small simulated data set with three omics and one categorical variable. 
