79902b0a11cd0b011181f90efab9cb4e80e90cb6
agugonrey <agugonrey@gmail.com>
2020-06-11 18:12:00 -0400

    Fix rrr tests

--- tests/testthat/test_rrr.R
+++ tests/testthat/test_rrr.R
@@ -1,25 +1,9 @@
 context("Reduced rank regression")
 
-test_that("Testing if MOSS gives same results than matrix general LM (non-singular case)", {
+test_that("Testing if MOSS gives same results than matrix general LM", {
   set.seed(42)
-  X <- matrix(rnorm(1000),100,10)
-  y <- X[,1:5] %*% rep(50,5) + rnorm(100,sd=0.05)
-  
-  b.prod <- crossprod(solve(crossprod(X)), crossprod(X,y))
-  
-  b.moss <- moss(data.blocks = list(y,X),scale. = F,
-                 resp.block = 1,
-                 method = 'rrr',
-                 K.X=10,
-                 K.Y = 1)$B
-  
-  expect_equal(b.prod , b.moss)
-})
-
-test_that("Testing if MOSS gives same results than matrix general LM (singular case)", {
-  set.seed(42)
-  X <- matrix(rnorm(1000),10,100)
-  y <- X[,1:5] %*% rep(50,5) + rnorm(10,sd=0.05)
+  X <- matrix(stats::rnorm(1000),10,100)
+  y <- X[,1:5] %*% rep(50,5) + stats::rnorm(10,sd=0.05)
   
   b.prod <- crossprod(MASS::ginv(crossprod(X)), crossprod(X,y))
   
@@ -32,32 +16,16 @@ test_that("Testing if MOSS gives same results than matrix general LM (singular c
   expect_equal(b.prod , b.moss)
 })
 
-test_that("Testing if MOSS gives same results than matrix general LM (multivariate; singular case)", {
-  sim_data <- simulate_data()
-  sim_blocks <- sim_data$sim_blocks
-  
-  X <- as.matrix(sim_blocks$`Block 1`[,1:20])
-  y <- sim_blocks$`Block 3`[,1:5]
-  
-  b.prod <- crossprod(solve(crossprod(X)), crossprod(X,y))
-  
-  b.moss <- moss(data.blocks = list(y,X),scale. = F,
-                 resp.block = 1,
-                 method = 'rrr',
-                 K.X=min(dim(X)),
-                 K.Y = min(dim(X), dim(y)))$B
-  
-  expect_equal(b.prod , b.moss)
-})
 
-test_that("Testing if MOSS gives same results than matrix general LM (multivariate multiple; singular case)", {
+
+test_that("Testing if MOSS gives same results than matrix general LM (multivariate multiple regression)", {
   sim_data <- simulate_data()
   sim_blocks <- sim_data$sim_blocks
   
   X <- sim_blocks$`Block 1`
   y <- sim_blocks$`Block 3`
   
-  b.prod <- crossprod(solve(crossprod(X)), crossprod(X,y))
+  b.prod <- crossprod(MASS::ginv(crossprod(X)), crossprod(X,y))
   
   b.moss <- moss(data.blocks = list(y,X),scale. = F,
                  resp.block = 1,
